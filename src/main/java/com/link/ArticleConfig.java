9b27fb2b7366254f55181f8984cfbecfe8747f38
Dxh7788 <anan1215>
2018-03-05 16:07:49 +0800

    "Spring Boot微服务搭建-Mybatis整合"

---
+++ src/main/java/com/link/ArticleConfig.java
@@ -0,0 +1,39 @@
+package com.link;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+
+/**
+ * Copyright (C) 2017-2018 https://www.htouhui.com - A project by DWeixin
+ *
+ * @author xh.d
+ * @since 2018/3/5 14:50
+ */
+@Configuration
+@EnableAutoConfiguration
+public class ArticleConfig {
+
+    //mybatis配置数据源
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource primaryDataSource(){
+        return DataSourceBuilder.create().build();
+    }
+    //配置SessionFactory
+    @Bean
+    public SqlSessionFactory myBatisSqlSessionFactory(DataSource primaryDataSource) throws Exception{
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(primaryDataSource);
+        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:/com/link/db/mapper/*.xml"));
+        return bean.getObject();
+    }
+}
--- src/main/java/com/link/MicroArticleApplication.java
+++ src/main/java/com/link/MicroArticleApplication.java
@@ -1,7 +1,9 @@
 package com.link;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 /**
  * Copyright (C) 2017-2018 https://www.dw.com - A project by DWeixin
@@ -10,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 2018/3/5 14:39
  */
 @SpringBootApplication
+@EnableAutoConfiguration
+@Import(value = ArticleConfig.class)
 public class MicroArticleApplication {
 
     public static void main(String[] args) {
