c6d3974bc4b8560715aa5c51b20f97cf9ba515ca
Dxh7788 <anan1215>
2018-03-05 18:27:29 +0800

    "Spring Boot微服务搭建-Mybatis整合-微信配置"

--- src/test/java/com/link/entity/JdbcTest.java
+++ src/test/java/com/link/entity/JdbcTest.java
@@ -4,6 +4,7 @@ import com.link.ArticleConfig;
 import com.link.MicroArticleApplication;
 import com.link.service.UserService;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
