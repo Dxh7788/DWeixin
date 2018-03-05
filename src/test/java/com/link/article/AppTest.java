bdc6b243b441b63f43ede754f4bbc748b85d0160
Dxh7788 <anan1215>
2018-03-05 14:36:35 +0800

    "架构修改"

---
+++ src/test/java/com/link/article/AppTest.java
@@ -0,0 +1,39 @@
+package com.link.article;
+
+import junit.framework.TestSuite;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Create the test case
+     */
+    @Test
+    @Before
+    public void setUp(){
+
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    @Test
+    public void  mainTest(){
+
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
